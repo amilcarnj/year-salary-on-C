e41c2311a82677c84d6bd4980c3c6087bb772af4
Amilcar Ponce <59424015+amilcarnj@users.noreply.github.com>
2021-06-28 14:02:55 -0400

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    float a,b;
+    int sum = 0;
+    
+    cout<<"Enter how much you make per hour :?"<<endl;
+    cin>>a;
+    cout<<"How many weeks you get pay a year?"<<endl;
+    cin>>b;
+    
+    sum = a*b*40;
+    
+    cout<<"Your yearly salary is :"<<sum<<endl;
+}
 
